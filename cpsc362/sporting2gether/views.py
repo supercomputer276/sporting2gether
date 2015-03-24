fa63815cacd42be159e179f374de875bb0052099
unknown <supercomputer276@cs408-4a.acad.fullerton.edu>
2015-03-24 14:06:37 -0700

    OK maybe this time it'll update the main thing.

--- cpsc362/sporting2gether/views.py
+++ cpsc362/sporting2gether/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	context_dict = {'page_title': 'Index'}
-	return render(request, 'sporting2gether/index.html', context_dict)
+	context_dict = {'page_title': 'Index', 'page_template': 'sporting2gether/index.html'}
+	return render(request, 'sporting2gether/page.html', context_dict)
